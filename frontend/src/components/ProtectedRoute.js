4f13e818b851160bf8746a92862cf9eb4c31878d
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-08-19 09:22:45 +0500

    delete front

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import {Navigate} from "react-router-dom";
-import React from "react";
-
-const PrivateRoute = ({ loggedIn, children }) => {
-    return loggedIn ? children : <Navigate to="/sign-in" />;
-};
-
-export default PrivateRoute;
