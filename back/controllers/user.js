1c9259ac5f172e77f952a5d097cfcb3afe578606
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-08-19 05:05:44 +0000

    add env and change token

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -96,7 +96,7 @@ module.exports.login = (req, res) => {
   } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      res.send({ token: jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' }) });
+      res.send({ token: jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' }) });
     })
     .catch((err) => {
       res.status(401)
